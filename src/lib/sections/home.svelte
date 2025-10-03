309df6a9b1bc7ce265519b2177c0d24ad2b364bf
forsyth-os <23yqk@queensu.ca>
2025-10-03 16:33:32 -0400

    fix tables

--- src/lib/sections/home.svelte
+++ src/lib/sections/home.svelte
@@ -154,13 +154,13 @@
 			Simulations (FEA/CFD)
 		</li>
 		<li>
-			Control Systems (LabVIEW/Arduino C++)
+			Control Systems (LabVIEW/C++)
 		</li>
 		<li>
 			Electrical Schematics (KiCAD)
 		</li>
 		<li>
-			Performance Testing (DAQ & Signal Processing)
+			Performance Testing (DAQ)
 		</li>
 		<li>
 			Data Analysis (Python/MATLAB)
